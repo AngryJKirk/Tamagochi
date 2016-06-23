91292fb553aa1c583da12bc19b2211ff0ec20d7a
angrynaz <angrynaz@gmail.com>
2016-06-23 09:21:56 +0300

    Renamed for better reading quality

---
+++ src/angry/naz/Tamagochi.java
@@ -0,0 +1,24 @@
+package angry.naz;
+
+import java.io.IOException;
+import org.telegram.telegrambots.TelegramApiException;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Tamagochi {
+
+    public static void main(String[] args)   throws IOException, InterruptedException {
+        FSM fsm = new FSM("Lena");
+        fsm.setNextState();
+
+    }
+
+
+
+
+
+
+}
